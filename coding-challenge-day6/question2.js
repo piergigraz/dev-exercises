d89fe825ae45072c13d00dc4151c1faaf49247b6
Pier <piergigraz@gmail.com>
2020-01-21 13:54:33 +0100

    returns as comment

--- coding-challenge-day6/question2.js
+++ coding-challenge-day6/question2.js
@@ -3,24 +3,21 @@
 const array = [1,5,3,3];
 
 const findCouple = (arr,sum) =>{
-                const addends = [];
+                let found =0;
                 for(let i=0 ; i<arr.length ; i++){
                     for(let j=0 ; j<arr.length ; j++){
                         if (i !== j && arr[i]+arr[j] === sum){
-                           addends.push([arr[i],arr[j]]);
-                          };
-                          // I could end the for loops once addends got a couple: 
-                          // if(addends.length > 0) {
-                          //   i = arr.length;
-                          //   j= arr.length;
-                          // };
-                        
+                          found++; 
+                          return console.log(`(${[arr[i],arr[j]]}) makes the sum requested: ${sum}`);
                         };
+                    };
                 };
-                if(addends.length == 0) {
-                  addends.push(`no numbers found`);
-                  };
-                return console.log(`(${addends[0]}) makes the sum requested: ${sum}`);
-  }
+                found === 0 ? console.log(`No number can make the sum requested`) : 0;
+            };
 
-  findCouple([1,2,6,4], 5);
+  // Return just the first addends found, console logging, without storing solutions
+  findCouple([1,2,4,1,2], 5);
+// (1,4) makes the sum requested: 5 
+
+findCouple([1,-1,2,5], 5);
+// No number can make the sum requested 
