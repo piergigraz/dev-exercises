e499bb3e62108a8a7ff332e55eba3f9fd0d300a6
Pier <piergigraz@gmail.com>
2020-05-09 20:38:38 +0200

    little change to try to commit

--- coding-challenge-day6/question2.js
+++ coding-challenge-day6/question2.js
@@ -3,7 +3,7 @@
 const array = [1,5,3,3];
 
 const findCouple = (arr,sum) =>{
-                let found =0;
+                let found = 0;
                 for(let i=0 ; i<arr.length ; i++){
                     for(let j=0 ; j<arr.length ; j++){
                         if (i !== j && arr[i]+arr[j] === sum){
